17965429698905874ee548a528a246466576384f
DHK <DHK@naver.com>
2020-07-31 14:33:18 +0900

    3rd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Fucking Fuck Shit Idiots!!!!!');
+    res.send('My love Moonyoung');
 });
 
 app.get('/start', function(req, res){
